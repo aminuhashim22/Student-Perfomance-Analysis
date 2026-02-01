857f6b2bcce44bd9a4b75b88a47d4bab073dcf04
aminuhashim22 <aminuhashim22@gmail.com>
2026-02-01 22:15:14 +0100

    Add files via upload

--- student_analysis.py
+++ student_analysis.py
@@ -11,14 +11,42 @@ st.write(df.describe())
 if st.button("Show Dataset"):
     st.dataframe(df)
 
-min_Age = st.slider("Select Minimum Age", 0, 10, 20, 30, 40, 50)
+min_Age = st.slider("Select Minimum Age", 0, 100, 50)
 filtered_df = df[df["Age"] >= min_Age]
 st.dataframe(filtered_df)
 
-uploaded_file = st.file_uploaded("Upload CSV file")
 
-if upoladed_file:
-    df = df.read_csv(uploaded_file)
+uploaded_file = st.file_uploader("Upload CSV file")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
     st.dataframe(df)
 
-st.bar_chart(df["Age"].value_counts())
+st.bar_chart(df["Score"].value_counts())
+
+st.line_chart(df["Score"])
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.hist(df["Score"])
+st.pyplot(fig)
+
+
+st.sidebar.title("Navigation")
+option = st.sidebar.selectbox(
+    "Choose View",
+    ["Dataset", "Summary", "Visualizations"]
+)
+
+
+if option == "Dataset":
+    st.dataframe(df)
+
+elif option == "Summary":
+    st.write(df.describe())
+
+elif option == "Visualizations":
+    st.bar_chart(df["Gender"].value_counts())
+
+
